2b7e8cf0e5a491918e6a12ddd48143d18a9fc20a
abstract829 <maxxii.1820@gmail.com>
2022-01-02 20:17:55 -0300

    db change

--- db/config.js
+++ db/config.js
@@ -1,15 +1,16 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 const pool = new Pool({
-    // connectionString : 'postgres://oqzawffryynovx:e58b480deda207affbfe4bdc8d5cc257c709214be21ae40871070e4c654b915d@ec2-54-147-203-50.compute-1.amazonaws.com:5432/d7685c1sk3jat',
-    // ssl:{
-    //     rejectUnauthorized: false
-    // }
-    host: 'localhost',
-    user: 'postgres',
-    password: '123',
-    database: 'proyectoFinalDB',
-    port: '5432'
-})
+  connectionString:
+    "postgres://oqzawffryynovx:e58b480deda207affbfe4bdc8d5cc257c709214be21ae40871070e4c654b915d@ec2-54-147-203-50.compute-1.amazonaws.com:5432/d7685c1sk3jat",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // host: 'localhost',
+  // user: 'postgres',
+  // password: '123',
+  // database: 'proyectoFinalDB',
+  // port: '5432'
+});
 module.exports = {
-    pool
-}
+  pool,
+};
